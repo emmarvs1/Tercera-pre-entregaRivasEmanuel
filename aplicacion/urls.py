de971aaca927804d4ad076555134992eba649270
Emanuel Rivas <emanuelrrivas@hotmail.com>
2024-02-07 23:48:19 -0300

    modif admin

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', home, name="home"),
     path('pizzas/', ver_pizzas, name="pizzas"),
     path('empanadas/', ver_empandas, name="empanadas"),
